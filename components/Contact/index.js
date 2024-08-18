138228e47655f9e8d8584249b89aef253e22dec8
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-18 10:53:06 -0500

    Created a contact form that stores the entries in a log file on server.

---
+++ components/Contact/index.js
@@ -0,0 +1,114 @@
+"use client";
+
+import toast from "react-hot-toast";
+import NewsLatterBox from "./NewsLatterBox";
+import { storeForm } from "./storeForm";
+import { useState } from "react";
+
+const Contact = () => {
+  const [sendStatus, setSendStatus] = useState(false);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setSendStatus(true);
+    await storeForm(
+      e.target.name.value,
+      e.target.email.value,
+      e.target.message.value
+    );
+
+    toast.success("Message successfully sent!");
+    setSendStatus(false);
+
+    e.target.name.value = "";
+    e.target.email.value = "";
+    e.target.message.value = "";
+  }
+
+  return (
+    <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div
+              className="wow fadeInUp shadow-three dark:bg-gray-dark mb-12 rounded-sm bg-white px-8 py-11 sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              data-wow-delay=".15s
+              "
+            >
+              <h2 className="mb-10 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                Need Help? Fill out this quick form
+              </h2>
+              <form onSubmit={handleSubmit}>
+                <div className="-mx-4 flex flex-wrap">
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="name"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        disabled={sendStatus}
+                        placeholder="Enter your name"
+                        className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        disabled={sendStatus}
+                        placeholder="Enter your email"
+                        className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="message"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Message
+                      </label>
+                      <textarea
+                        name="message"
+                        rows={5}
+                        placeholder="Enter your Message"
+                        disabled={sendStatus}
+                        className="border-stroke dark:text-body-color-dark dark:shadow-two w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <button
+                      className="shadow-submit dark:shadow-submit-dark rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90"
+                      disabled={sendStatus}
+                    >
+                      {sendStatus ? "Sending..." : "Submit"}
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+            <NewsLatterBox />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
