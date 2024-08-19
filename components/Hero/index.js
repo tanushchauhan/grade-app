9c60a7661f48a4e2255e5891f9970f0cb7eb393d
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-19 00:49:41 -0500

    Added the hero to homepage.

---
+++ components/Hero/index.js
@@ -0,0 +1,180 @@
+import Btn from "./Btn";
+import ClientLink from "./ClientLink";
+
+const Hero = () => {
+  return (
+    <>
+      <section
+        id="home"
+        className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+      >
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div
+                className="wow fadeInUp mx-auto max-w-[800px] text-center"
+                data-wow-delay=".2s"
+              >
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  GradeMate
+                </h1>
+                <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
+                  GradeMate is your ultimate academic companion, designed to
+                  simplify your journey through school. GradeMate ensures you
+                  have all the tools you need to excel. Say goodbye to
+                  grade-related stress and hello to a smoother, more organized
+                  academic experience with GradeMate by your side.
+                </p>
+                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <ClientLink />
+                  <Btn />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute right-0 top-0 z-[-1] opacity-30 lg:opacity-100">
+          <svg
+            width="450"
+            height="556"
+            viewBox="0 0 450 556"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="277"
+              cy="63"
+              r="225"
+              fill="url(#paint0_linear_25:217)"
+            />
+            <circle
+              cx="17.9997"
+              cy="182"
+              r="18"
+              fill="url(#paint1_radial_25:217)"
+            />
+            <circle
+              cx="76.9997"
+              cy="288"
+              r="34"
+              fill="url(#paint2_radial_25:217)"
+            />
+            <circle
+              cx="325.486"
+              cy="302.87"
+              r="180"
+              transform="rotate(-37.6852 325.486 302.87)"
+              fill="url(#paint3_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="184.521"
+              cy="315.521"
+              r="132.862"
+              transform="rotate(114.874 184.521 315.521)"
+              stroke="url(#paint4_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="356"
+              cy="290"
+              r="179.5"
+              transform="rotate(-30 356 290)"
+              stroke="url(#paint5_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="191.659"
+              cy="302.659"
+              r="133.362"
+              transform="rotate(133.319 191.659 302.659)"
+              fill="url(#paint6_linear_25:217)"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_25:217"
+                x1="-54.5003"
+                y1="-178"
+                x2="222"
+                y2="288"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <radialGradient
+                id="paint1_radial_25:217"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(17.9997 182) rotate(90) scale(18)"
+              >
+                <stop offset="0.145833" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0.08" />
+              </radialGradient>
+              <radialGradient
+                id="paint2_radial_25:217"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(76.9997 288) rotate(90) scale(34)"
+              >
+                <stop offset="0.145833" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0.08" />
+              </radialGradient>
+              <linearGradient
+                id="paint3_linear_25:217"
+                x1="226.775"
+                y1="-66.1548"
+                x2="292.157"
+                y2="351.421"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint4_linear_25:217"
+                x1="184.521"
+                y1="182.159"
+                x2="184.521"
+                y2="448.882"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="white" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint5_linear_25:217"
+                x1="356"
+                y1="110"
+                x2="356"
+                y2="470"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="white" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint6_linear_25:217"
+                x1="118.524"
+                y1="29.2497"
+                x2="166.965"
+                y2="338.63"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+      </section>
+      <a id="ahead"></a>
+    </>
+  );
+};
+
+export default Hero;
