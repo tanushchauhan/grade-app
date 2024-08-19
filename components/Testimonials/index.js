ada546ea1b1a80265c1bc6223e5eba25fc66458e
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-19 16:44:13 -0500

    Added the Testimonials.

---
+++ components/Testimonials/index.js
@@ -0,0 +1,86 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleTestimonial from "./SingleTestimonial";
+
+const testimonialData = [
+  {
+    id: 1,
+    name: "Punith",
+    designation: "Senior, Lebanon Trail High School",
+    content:
+      "I actually quite like the app. The best part is that the gpa estimation is free.",
+    image: "/images/testimonials/auth-1.png",
+    star: 5,
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section className="relative z-10 bg-gray-light py-16 dark:bg-bg-color-dark md:py-20 lg:py-28">
+      <div className="container">
+        <SectionTitle title="What Our Users Says" center />
+
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
+          {testimonialData.map((testimonial) => (
+            <SingleTestimonial key={testimonial.id} testimonial={testimonial} />
+          ))}
+        </div>
+      </div>
+      <div className="absolute right-0 top-5 z-[-1]">
+        <svg
+          width="238"
+          height="531"
+          viewBox="0 0 238 531"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            opacity="0.3"
+            x="422.819"
+            y="-70.8145"
+            width="196"
+            height="541.607"
+            rx="2"
+            transform="rotate(51.2997 422.819 -70.8145)"
+            fill="url(#paint0_linear_83:2)"
+          />
+          <rect
+            opacity="0.3"
+            x="426.568"
+            y="144.886"
+            width="59.7544"
+            height="541.607"
+            rx="2"
+            transform="rotate(51.2997 426.568 144.886)"
+            fill="url(#paint1_linear_83:2)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_83:2"
+              x1="517.152"
+              y1="-251.373"
+              x2="517.152"
+              y2="459.865"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4A6CF7" />
+              <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+            </linearGradient>
+            <linearGradient
+              id="paint1_linear_83:2"
+              x1="455.327"
+              y1="-35.673"
+              x2="455.327"
+              y2="675.565"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4A6CF7" />
+              <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
