846748bd92640c95372fc708e09d48df206a0631
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-26 10:18:40 -0500

    Added some JSX to the home of dashboard.

--- app/dashboard/home/Com.js
+++ app/dashboard/home/Com.js
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { globalContext } from "@/app/providers";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 function AssignmentItem({
   data,
@@ -315,7 +317,96 @@ function Com({ params }) {
     setAssignmentsAdded((e) => e + 1);
   }
 
-  return <div></div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.75 }}
+    >
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none m-2 text-slate-200">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div
+                className="backdrop-blur-sm fixed inset-0 z-40"
+                onClick={() => updateModalStatus(false)}
+              ></div>
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-700 outline-none focus:outline-none z-[55]">
+                {/*header*/}
+                <div className="flex items-center justify-between p-5  rounded-t">
+                  <h3 className="text-3xl font-semibold">Assignemnt Details</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => updateModalStatus(false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      className="w-14 h-14 stroke-slate-200"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18 18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <div className="flex flex-col gap-5">
+                    <div className="grid grid-cols-2 gap-x-8 gap-y-4 my-2">
+                      <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
+                        <p className="font-bold text-center">Date Assigned</p>
+                        <p className="text-xl  text-slate-100 font-semibold text-center">
+                          {modalData[1].trim() === "" ? "None" : modalData[1]}
+                        </p>
+                      </div>
+                      <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
+                        <p className="font-bold text-center">Date Due</p>
+                        <p className="text-xl  text-slate-100 font-semibold text-center">
+                          {modalData[0].trim() === "" ? "None" : modalData[0]}
+                        </p>
+                      </div>
+                      <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
+                        <p className="font-bold text-center">Score</p>
+                        <p className="text-xl  text-slate-100 font-semibold text-center">
+                          {modalData[4].trim() === "" ? "None" : modalData[4]} /{" "}
+                          {modalData[5]}
+                        </p>
+                      </div>
+                      <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
+                        <p className="font-bold text-center">Weighted Points</p>
+                        <p className="text-xl  text-slate-100 font-semibold text-center">
+                          {modalData[7].trim() === "" ? "None" : modalData[7]} /{" "}
+                          {modalData[8]}
+                        </p>
+                      </div>
+                      <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
+                        <p className="font-bold text-center">Weight</p>
+                        <p className="text-xl  text-slate-100 font-semibold text-center">
+                          {modalData[6].trim() === "" ? "None" : modalData[6]}
+                        </p>
+                      </div>
+                      <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
+                        <p className="font-bold text-center">Percentage</p>
+                        <p className="text-xl  text-slate-100 font-semibold text-center">
+                          {Number(modalData[9]) ? Number(modalData[9]) : "0.0"}%
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </motion.div>
+  );
 }
 
 export default Com;
