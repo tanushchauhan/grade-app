a213a4878de790c467e4f46839e359639da8228a
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-26 12:19:52 -0500

    Added some more JSX

--- app/dashboard/home/Com.js
+++ app/dashboard/home/Com.js
@@ -1,7 +1,19 @@
-import { useContext, useEffect, useRef, useState } from "react";
+"use client";
+
 import { globalContext } from "@/app/providers";
+import ThemeToggler from "@/components/Header/ThemeToggler";
+import { deleteCookie } from "cookies-next";
+import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
+
+import { usePathname, useRouter } from "next/navigation";
+import { useContext, useEffect, useRef, useState } from "react";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function AssignmentItem({
   data,
@@ -358,6 +370,14 @@ function Com({ params }) {
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <div className="flex flex-col gap-5">
+                    <div className="flex items-center justify-center flex-col gap-2 bg-primary p-3 rounded-3xl z-50">
+                      <p className="font-bold text-center">
+                        Type of Assignment
+                      </p>
+                      <p className="text-xl  text-slate-100 font-semibold text-center">
+                        {modalData[3]}
+                      </p>
+                    </div>
                     <div className="grid grid-cols-2 gap-x-8 gap-y-4 my-2">
                       <div className="flex items-center flex-col gap-2 justify-center bg-primary p-3 rounded-3xl">
                         <p className="font-bold text-center">Date Assigned</p>
@@ -405,6 +425,129 @@ function Com({ params }) {
           </div>
         </>
       ) : null}
+
+      <section className="relative z-10 overflow-hidden pt-7">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap items-center">
+            <div className="w-full px-4 md:w-8/12 lg:w-7/12">
+              <div className="mb-8 max-w-[570px] md:mb-0 lg:mb-12">
+                <h1 className="mb-5 text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                  {data?.courseName}
+                </h1>
+
+                <button
+                  className="bg-primary px-4 py-2 text-white hover:bg-primary/90"
+                  onClick={() => params.update("home")}
+                >
+                  Go Back To Dashboard
+                </button>
+              </div>
+            </div>
+            <div className="w-full px-4 md:w-4/12 lg:w-5/12">
+              <div className="text-end">
+                <ul className="flex items-center md:justify-end">
+                  <li className="flex items-center">
+                    <Link
+                      href="/"
+                      className="pr-1 text-base font-medium text-body-color hover:text-primary"
+                    >
+                      Home
+                    </Link>
+                    <span className="mr-3 block h-2 w-2 rotate-45 border-r-2 border-t-2 border-body-color"></span>
+                  </li>
+                  <li className="flex items-center">
+                    <Link
+                      href="/dashboard"
+                      className="pr-1 text-base font-medium text-body-color hover:text-primary"
+                    >
+                      Dashboard
+                    </Link>
+                    <span className="mr-3 block h-2 w-2 rotate-45 border-r-2 border-t-2 border-body-color"></span>
+                  </li>
+                  <li className="text-base font-medium text-primary">
+                    {params.id}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <span className="absolute left-0 top-0 z-[-1]">
+            <svg
+              width="287"
+              height="254"
+              viewBox="0 0 287 254"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                opacity="0.1"
+                d="M286.5 0.5L-14.5 254.5V69.5L286.5 0.5Z"
+                fill="url(#paint0_linear_111:578)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_111:578"
+                  x1="-40.5"
+                  y1="117"
+                  x2="301.926"
+                  y2="-97.1485"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#4A6CF7" />
+                  <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+          <span className="absolute right-0 top-0 z-[-1]">
+            <svg
+              width="628"
+              height="258"
+              viewBox="0 0 628 258"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                opacity="0.1"
+                d="M669.125 257.002L345.875 31.9983L524.571 -15.8832L669.125 257.002Z"
+                fill="url(#paint0_linear_0:1)"
+              />
+              <path
+                opacity="0.1"
+                d="M0.0716344 182.78L101.988 -15.0769L142.154 81.4093L0.0716344 182.78Z"
+                fill="url(#paint1_linear_0:1)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_0:1"
+                  x1="644"
+                  y1="221"
+                  x2="429.946"
+                  y2="37.0429"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#4A6CF7" />
+                  <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                </linearGradient>
+                <linearGradient
+                  id="paint1_linear_0:1"
+                  x1="18.3648"
+                  y1="166.016"
+                  x2="105.377"
+                  y2="32.3398"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#4A6CF7" />
+                  <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+        </div>
+      </section>
     </motion.div>
   );
 }
