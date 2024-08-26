2bb8fe10aa9db7b9031334219a743208653a9efc
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-26 00:46:00 -0500

    Added some more courses data scrapped from the catalog.

--- app/api/hac/courses/route.js
+++ app/api/hac/courses/route.js
@@ -656,6 +656,1034 @@ const classes = {
     weight: 5.5,
     multiplier: 1,
   },
+  COLM0430: {
+    name: "Collin Dual Credit Pre-Calculus",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0440: {
+    name: "Collin Dual Credit Calculus I",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0450: {
+    name: "Collin Dual Credit Calculus III",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  COLM0460: {
+    name: "Collin Dual Credit Differential Equations",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  COLM0500: {
+    name: "Collin Dual Credit Biology for Non-Science Majors",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0510: {
+    name: "Collin Dual Credit Biology for Science Majors",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0600: {
+    name: "Collin Dual Credit US Government",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0610: {
+    name: "Collin Dual Credit Microeconomics",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0620: {
+    name: "Collin Dual Credit Texas Government",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0630: {
+    name: "Collin Dual Credit Sociology",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0640: {
+    name: "Collin Dual Credit US History",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0650: {
+    name: "Collin Dual Credit Learning Framework",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  COLM0700: {
+    name: "Collin Dual Credit Intro to Speech Communication",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELA11100: {
+    name: "English I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA11200: {
+    name: "English I Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELA11500: {
+    name: "GT Humanities I/English I Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELA12100: {
+    name: "English II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA12200: {
+    name: "English II Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELA13100: {
+    name: "English III",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA13300: {
+    name: "AP English Language & Composition",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  ELA14100: {
+    name: "English IV",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA14300: {
+    name: "AP English Literature & Composition",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  ELA21100: {
+    name: "Debate I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA21200: {
+    name: "Oral Interpretation I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA22100: {
+    name: "Debate II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA22200: {
+    name: "Oral Interpretation II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA23100: {
+    name: "Debate III",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA25500: {
+    name: "Professional Communication",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA25600: {
+    name: "Advanced Public Speaking",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA35100: {
+    name: "Creative and Imaginative Writing",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA41100: {
+    name: "Journalism",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA41600: {
+    name: "Yearbook I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA42600: {
+    name: "Advanced Journalism/Yearbook II-III",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA43110: {
+    name: "Newspaper I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA43111: {
+    name: "Newspaper 2",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA43112: {
+    name: "Newspaper 3",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA43600: {
+    name: "Yearbook 3",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA43601: {
+    name: "Independent Study in Journalism",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA44100: {
+    name: "Photojournalism",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELA71000: {
+    name: "GT American Studies/AP English Language & Composition",
+    weight: 6.0,
+    multiplier: 1,
+    name: "! THIS IS SET TO MULTIPLIER 1 BECAUSE IT SHOWS UP IN HAC TWICE ANYWAY",
+  },
+  ELA85300: {
+    name: "AP Research",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  ELC11100: {
+    name: "LeadWorthy 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC11120: {
+    name: "Academic Decathlon I",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELC11140: {
+    name: "Academic Decathlon II",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELC11160: {
+    name: "Academic Decathlon III",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELC11170: {
+    name: "Academic Decathlon IV",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELC12100: {
+    name: "LeadWorthy 2",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC12110: {
+    name: "Student Congress 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC12200: {
+    name: "AVID 9th Grade (Advancement Via Individual Determination)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC12300: {
+    name: "AVID 10th Grade (Advancement Via Individual Determination)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC12400: {
+    name: "AVID 11th Grade (Advancement Via Individual Determination)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC12500: {
+    name: "AVID 12th Grade (Advancement Via Individual Determination)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC14115: {
+    name: "Independent Study and Mentorship Year 1",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELC14125: {
+    name: "Independent Study and Mentorship Year 2",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  ELC21000: {
+    name: "Peer Assistance & Leadership (PALS/Student Ambassadors)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC22000: {
+    name: "Peer Assistance & Leadership (PALS/Student Ambassadors)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  ELC55110: {
+    name: "Sports Medicine",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG11100: {
+    name: "French 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG12100: {
+    name: "French 2",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG13200: {
+    name: "French 3 Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FLG14300: {
+    name: "French 4 AP",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FLG21100: {
+    name: "Spanish 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG22100: {
+    name: "Spanish 2",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG23200: {
+    name: "Spanish 3 Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FLG24300: {
+    name: "Spanish 4 AP",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FLG25300: {
+    name: "Spanish 5 AP",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FLG41100: {
+    name: "American Sign Language 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG41101: {
+    name: "American Sign Language 2",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG61100: {
+    name: "Chinese 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG62100: {
+    name: "Chinese 2",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FLG63100: {
+    name: "Chinese 3 Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FLG64300: {
+    name: "Chinese 4 AP",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FNA11100: {
+    name: "Art I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA11150: {
+    name: "Art I Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FNA12100: {
+    name: "Art II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA12102: {
+    name: "Art II Ceramics",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA12200: {
+    name: "Art II Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FNA13100: {
+    name: "Art III",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FNA14100: {
+    name: "Art IV",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  FNA14300: {
+    name: "AP Studio Art - Drawing",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FNA14310: {
+    name: "AP Art History",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FNA14320: {
+    name: "AP Studio Art - 2D Design",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FNA14330: {
+    name: "AP Studio Art - 3D Design",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FNA25101: {
+    name: "Band",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA25201: {
+    name: "Instrumental Ensemble: Band",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA25914: {
+    name: "Choir",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA26101: {
+    name: "Orchestra",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA26205: {
+    name: "Instrumental Ensemble: Orchestra",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA31113: {
+    name: "Vocal Ensemble",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA41100: {
+    name: "Theatre Arts I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA41101: {
+    name: "Intermediate Theatre Arts I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA41102: {
+    name: "Technical Theatre I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA41104: {
+    name: "Theatre Arts II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA41105: {
+    name: "Technical Theatre II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA41121: {
+    name: "Intermediate Technical Theatre I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA51101: {
+    name: "Dance I - Fundamentals",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA51110: {
+    name: "Color Guard 1 YR",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA52101: {
+    name: "Dance II - Technique",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA52110: {
+    name: "Color Guard 2 YR",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA53101: {
+    name: "Dance III - Intermediate",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA53110: {
+    name: "Color Guard 3 YR",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA54101: {
+    name: "Dance IV - Advanced",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA54110: {
+    name: "Color Guard 4 YR",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  FNA65300: {
+    name: "AP Music Theory",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  FNA65400: {
+    name: "Music Theory 1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  HPE45100: {
+    name: "Health Education",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  IB011001: {
+    name: "IB Theory of Knowledge Part 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB011002: {
+    name: "IB Theory of Knowledge Part 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB22100Y: {
+    name: "IB Mathematic: applications and interpretation SL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB22120Y: {
+    name: "IB Mathematic: applications and interpretation SL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB22200Y: {
+    name: "IB Mathematic: analysis and approaches HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB22220Y: {
+    name: "IB Mathematic: analysis and approaches HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB31100Y: {
+    name: "IB History SL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB31100Y: {
+    name: "IB History SL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB31200Y: {
+    name: "IB History of the Americas HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB31200Y: {
+    name: "IB History of the Americas HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB33100Y: {
+    name: "IB Psychology SL",
+    weight: 6.0,
+    multiplier: 2,
+  },
+  IB41100Y: {
+    name: "IB French SL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB41120Y: {
+    name: "IB French SL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB42100Y: {
+    name: "IB Spanish SL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB42120Y: {
+    name: "IB Spanish SL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB42200Y: {
+    name: "IB Spanish HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB42220Y: {
+    name: "IB Spanish HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB51100Y: {
+    name: "IB Visual Arts SL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB51100Y: {
+    name: "IB Visual Arts SL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB51200Y: {
+    name: "IB Visual Arts HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB51200Y: {
+    name: "IB Visual Arts HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB52100Y: {
+    name: "IB Music SL",
+    weight: 6.0,
+    multiplier: 2,
+  },
+  IB61200Y: {
+    name: "IB Biology HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB61220Y: {
+    name: "IB Biology HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB62100Y: {
+    name: "IB Environmental Systems and Societies SL",
+    weight: 6.0,
+    multiplier: 2,
+  },
+  IB63100Y: {
+    name: "IB Physics SL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB63120Y: {
+    name: "IB Physics SL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB63200Y: {
+    name: "IB Physics HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB63220Y: {
+    name: "IB Physics HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB71200Y: {
+    name: "IB Language and Literature HL Year 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  IB71220Y: {
+    name: "IB Language and Literature HL Year 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  MTH11100: {
+    name: "Algebra I",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  MTH11200: {
+    name: "Algebra I Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  MTH12100: {
+    name: "Algebra II",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  MTH12200: {
+    name: "Algebra II Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  MTH23100: {
+    name: "Geometry",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  MTH23200: {
+    name: "Geometry Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  MTH34100: {
+    name: "Precalculus",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  MTH34200: {
+    name: "Precalculus Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  MTH36100: {
+    name: "AQR",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  MTH37100: {
+    name: "Statistics",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  MTH45300: {
+    name: "AP Calculus AB",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  MTH45305: {
+    name: "AP Calculus BC",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  MTH45310: {
+    name: "AP Statistics",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  MTH52100: {
+    name: "Algebraic Reasoning",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  PEC01001: {
+    name: "Cheerleading EQ1",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  PEF02001: {
+    name: "Partners PE",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  PELF1001: {
+    name: "Lifetime Fitness and Wellness",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  PELR1001: {
+    name: "Lifetime Recreation and Outdoor Education",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  PESB1001: {
+    name: "Skills-Based Activities",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI11100: {
+    name: "Biology",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI11200: {
+    name: "Biology Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  SCI13300: {
+    name: "AP Biology",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SCI22100: {
+    name: "Chemistry",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI22200: {
+    name: "Chemistry Advanced",
+    weight: 5.5,
+    multiplier: 1,
+  },
+  SCI24300: {
+    name: "AP Chemistry",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SCI33100: {
+    name: "Physics",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI34400: {
+    name: "AP Physics 1",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SCI34500: {
+    name: "AP Physics 2",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SCI35500: {
+    name: "AP Physics C",
+    weight: 6.0,
+    multiplier: 2,
+  },
+  SCI42100: {
+    name: "Integrated Physics & Chemistry (IPC)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43100: {
+    name: "Environmental Science",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43110: {
+    name: "Anatomy & Physiology",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43120: {
+    name: "Aquatic Science",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43131: {
+    name: "Earth Systems Science (ESS)",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43145: {
+    name: "Medical Microbio@CTEC",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43155: {
+    name: "Pathophysiology Yr@CTEC",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SCI43300: {
+    name: "AP Environmental Science",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SCI44100: {
+    name: "Astronomy",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST11100: {
+    name: "World Geography",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST11500: {
+    name: "AP Human Geography",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST22100: {
+    name: "World History",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST22300: {
+    name: "AP World History",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST22400: {
+    name: "GT Humanities II/AP World History",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST23100: {
+    name: "US History",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST23300: {
+    name: "AP US History",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST23310: {
+    name: "AP European History",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST23400: {
+    name: "African American Studies",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST23500: {
+    name: "Mexican American Studies",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST34100: {
+    name: "Government",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST34110: {
+    name: "Economics",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST34120: {
+    name: "Personal Financial Literacy and Economics",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST34300: {
+    name: "AP Government",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST34310: {
+    name: "AP Macroeconomics",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST45100: {
+    name: "Sociology",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST45110: {
+    name: "Psychology",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST45300: {
+    name: "AP Psychology- 12th Grade Only",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST45500: {
+    name: "AP Psychology with Social Studies Research",
+    weight: 6.0,
+    multiplier: 1,
+    weight2: 5.0,
+    name: "! THIS IS 5.0 FIRST SEM AND 6.0 SECOND SEM, IDK HOW U GONNA CODE THAT",
+  },
+  SST65100: {
+    name: "Personal Financial Literacy",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SST85300: {
+    name: "AP Seminar",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SST85350: {
+    name: "AP Seminar: Multicultural Section",
+    weight: 6.0,
+    multiplier: 1,
+  },
+  SUBA1001: {
+    name: "Baseball 9th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1002: {
+    name: "Baseball 10th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1003: {
+    name: "Baseball 11th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1004: {
+    name: "Baseball 12th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1101: {
+    name: "Basketball 9th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1102: {
+    name: "Basketball 10th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1103: {
+    name: "Basketball 11th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
+  SUBA1104: {
+    name: "Basketball 12th Grade",
+    weight: 5.0,
+    multiplier: 1,
+  },
 };
 
 export async function GET() {
